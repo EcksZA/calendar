f008a8e5263b7adbe8e05e47e856daf68e8b5746
Xolani Mfuko & Amy Vaillancourt-Sals <student@epicodus.com>
2014-08-21 11:15:03 -0700

    validate presence of end_date

--- lib/event.rb
+++ lib/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   validates :description, presence: true
   validates :location, presence: true
   validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,4 +4,5 @@ describe Event do
   it { should validate_presence_of :description }
   it { should validate_presence_of :location }
   it { should validate_presence_of :start_date}
+  it { should validate_presence_of :end_date}
 end
