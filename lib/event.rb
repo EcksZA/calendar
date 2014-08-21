fb368e301f4c6882a1a0c89050ade37cfa03f55d
Xolani Mfuko & Amy Vaillancourt-Sals <student@epicodus.com>
2014-08-21 13:36:33 -0700

    add method to sort events by start_date

--- lib/event.rb
+++ lib/event.rb
@@ -3,4 +3,12 @@ class Event < ActiveRecord::Base
   validates :location, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+
+  def self.list_order
+    Event.all.sort_by(&:start_date)
+    # x >= Date.today
+    # Event.where("start_date = ?, x")
+    # end
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,4 +5,11 @@ describe Event do
   it { should validate_presence_of :location }
   it { should validate_presence_of :start_date}
   it { should validate_presence_of :end_date}
+
+  it 'lists out events in order of soonest' do
+    event1 = Event.create({description: "Get lunch", location: "work", start_date: '23rd Aug 2014 12:00:00', end_date: '14th Sep 2014 12:00:00'})
+    event2 = Event.create({description: "Get lunch", location: "work", start_date: '20rd Aug 2015 12:00:00', end_date: '14th Nov 2015 12:00:00'})
+    event3 = Event.create({description: "Get lunch", location: "work", start_date: '20rd Mar 2015 12:00:00', end_date: '14th Apr 2015 12:00:00'})
+    expect(Event.list_order).to eq [event1, event3, event2]
+  end
 end
